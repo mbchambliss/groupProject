8be00030be8f3a3906968585a2121b9f2cb8d174
GroupBcomp324 <36512887+GroupBcomp324@users.noreply.github.com>
2018-03-08 21:58:56 -0600

    CSS and JQuery updates 3-8-17
    Minor updates to CSS and JQuery

--- projectJS.js
+++ projectJS.js
@@ -9,9 +9,9 @@ function clearResults2() {
     this.checked = false;
     });
     $('#submitButton').show();
+    $('#results').hide();
 });};
 document.getElementById('resetButton').onclick = clearResults2;
-
 
 function showDeals1() {
     $(function(){
@@ -41,6 +41,11 @@ document.getElementById('staff').onclick = showDeals4;
 // status = 'closed'; // set the default menu status
 
 function checkResults(){
+    var styles = {
+        "height": "auto",
+        "border-style": "solid"
+    };
+    $('#results').show().css(styles);
     $('#submitButton').hide();
     checkFood();
     checkAge();
@@ -51,40 +56,37 @@ document.getElementById('submitButton').onclick = checkResults;
 var notOfAge = "You must be 21 years of age to drink. No drink specials available.";
 
 function checkAge() {
-var elDrink = document.getElementById('ageChoice2').checked;
-if (elDrink === true){
-    $('#results').append(notOfAge);
-}
-
+    var elDrink = document.getElementById('ageChoice2').checked;
+    if (elDrink === true){
+        $('#results').append(notOfAge);
+    }
 };
 
 function checkFood(){
 var FoodChoice = document.getElementById('activityChoice1').checked;
 var StudentChoice = document.getElementById('studentChoice1').checked;
     if (FoodChoice === true && StudentChoice === true ) {
-        $('#results').append(Foodrecommend2);
+        $('#results').append(Foodrecommend2, Foodrecommend1);
     }
     else if (FoodChoice === true && StudentChoice === false) {
         $('#results').append(Foodrecommend1);
-    } else {
-        $('#results').append("No food recommendations requested.");
     }
 };
 
-var Foodrecommend1 = "Flaco's Tacos is what we recommend, here is the address: 1116-20 West Granville, check their website for details.";
+var Foodrecommend1 = "Flaco's Tacos is what we recommend, here is the address: 1116-20 West Granville, check their website for details. ";
 //need to show URL as clickable
-var Foodrecommend2 = "Veranda Restaurant & Coffee, it gives 10% off to Loyola students with valid Student ID."
+var Foodrecommend2 = "Veranda Restaurant & Coffee, it gives 10% off to Loyola students with valid Student ID. ";
 
 
-var Eventrecommend1 = "Men's Volleyball vs Ball State.";
-var Eventrecommend2 = "Loyola Fine Arts Exibition.";
-var CommunityEvent = "Mosaic Art Classes Available.";
+var Eventrecommend1 = "Men's Volleyball vs Ball State. ";
+var Eventrecommend2 = "Loyola Fine Arts Exibition. ";
+var CommunityEvent = "Mosaic Art Classes Available. ";
 
-function checkEvent(){
-var eventChoice = document.getElementById('activityChoice3').checked;
-// var dateChoice = document.getElementById('date').checked;
-
-if (eventChoice === true)
-{
-    $('#results').append(Eventrecommend1, Eventrecommend2, CommunityEvent);
-}};
+function checkEvent() {
+    var eventChoice = document.getElementById('activityChoice3').checked;
+    // var dateChoice = document.getElementById('date').checked;
+    if (eventChoice === true)
+    {
+        $('#results').append(Eventrecommend1, Eventrecommend2, CommunityEvent);
+    }
+};
